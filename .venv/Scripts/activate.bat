0500badb5b66b23309da58db7642565cfe3fad1f
Gabriel Batista <gabrielucasbh2011@gmail.com>
2023-10-12 16:11:30 -0300

    Commit Teste

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Gabriel Batista\Documents\P I A - Projeto Integrato de Aplicacoes\DjangoChopp\.venv
+set VIRTUAL_ENV=C:\Users\Gabriel Batista\Documents\P I A - Projeto Integrato de Aplicacoes\DjangoChopp\DjangoChopp\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
